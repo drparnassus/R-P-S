75e3fa642261e163cefc8684703b7b0e788309ba
drparnassus <drparnassus@gmail.com>
2024-04-11 15:30:34 -0500

    cleaned up and removed code to comply with original project specs

--- script.js
+++ script.js
@@ -1,146 +1,75 @@
 /* script.js */
-let playerRWins = 0;
-let aiRWins = 0;
-let jsChoice = 0;
-let choice = 0;
-let playerMWins = 0;
-let aiMWins = 0;
-let pR = 0;
-let pP = 0;
-let pS = 0;
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
+let playerWins = 0;
+let computerWins = 0;
 
 function rngMaybe(min, max) {
-  let chance = Math.floor(Math.random() * 101);
-  if (chance <= 5) {
-    if (pR > pP && pR > pS) {
-      return 2;
-    }
-    else if (pP > pR && pP > pS) {
-      return 3;
-    }
-    else if (pS > pP && pS > pR) {
-      return 1;
-    }
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function getComputerChoice() {
+  let jsChoice = rngMaybe(1, 3)
+  if (jsChoice == 1) {
+    console.log("I picked " + rock + "!");
+    return rock;
+  }
+  else if (jsChoice == 2) {
+    console.log("I picked " + paper + "!");
+    return paper;
+  }
+  else if (jsChoice == 3) {
+    console.log("I picked " + scissors + "!");
+    return scissors;
+  }
+}
+
+function getPLayerSelection() { 
+  choice = prompt("rock, paper, or scissors?")
+  console.log("you picked " + choice + "!");
+  if (choice.toLowerCase() == rock) {
+    return rock;
+  }
+  if (choice.toLowerCase() == paper) {
+    return paper;
+  }
+  if (choice.toLowerCase() == scissors) {
+    return scissors;
+  }
+}
+
+function playGame() {
+  if (prompt("would you like to play a game?").toLowerCase() == "y") {
+    do {
+      const pChoice = getPLayerSelection();
+      const cChoice = getComputerChoice();
+      console.log(playRound(pChoice, cChoice));
+    } while (playerWins < 5 && computerWins <5);
+  }
+  if (playerWins == 5) {
+    console.log("you won the game...");
+  }
+  else if (computerWins == 5) {
+    console.log("you lost the game...");
+  }
+}
+
+function playRound(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return `it's a tie! the score is ${playerWins} to ${computerWins}`;
+  }
+  else if ((playerChoice === rock && computerChoice === paper) || (playerChoice === paper && computerChoice === scissors) || (playerChoice === scissors && computerChoice === rock)) {
+    ++computerWins;
+    return `you lose! ${computerChoice} beats ${playerChoice}! the score is ${playerWins} to ${computerWins}`;
   }
   else {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+    ++playerWins;
+    return `you win! ${playerChoice} beats ${computerChoice}! the score is ${playerWins} to ${computerWins}`
   }
 }
 
-function aiTurn() {
-  jsChoice = rngMaybe(1, 3)
-
-  console.log(jsChoice);
-  console.log("computer picked " + jsChoice + "!");
-  return jsChoice;
-}
+//console.log(playRound(getPLayerSelection(),getComputerChoice()));
 
 
-function playerTurn() {
-  choice = prompt("rock(1), paper(2), or scissors(3)?")
-  console.log("You picked " + choice + "!");
-  if (choice == 1) {
-    ++pR;
-  }
-  else if (choice == 2) {
-    ++pP;
-  }
-  else if (choice == 3) {
-    ++pS;
-  }
-  return choice;
-}
-
-function c2I(convertMe) {
-  if (convertMe == 1) {
-    return "rock";
-  }
-  else if (convertMe == 2) {
-    return "paper";
-  }
-  else if (convertMe == 3) {
-    return "scissors"
-  }
-}
-
-function compareChoices(pC, aiC) {
-  if (pC == aiC) {
-    console.log("You tied!")
-    return 3; //player tie
-  }
-  else if (pC == 1 && aiC == 2) {
-    console.log("You lost!")
-    ++aiRWins;
-    return 1; //player lose
-  }
-  else if (pC == 1 && aiC == 3) {
-    console.log("You won!")
-    ++playerRWins;
-    return 2; //player win
-  }
-  else if (pC == 2 && aiC == 1) {
-    console.log("You won!")
-    ++playerRWins;
-    return 2;
-  }
-  else if (pC == 2 && aiC == 3) {
-    console.log("You lost!")
-    ++aiRWins;
-    return 1;
-  }
-  else if (pC == 3 && aiC == 1) {
-    console.log("You lost!")
-    ++aiRWins;
-    return 1;
-  }
-  else if (pC == 3 && aiC == 2) {
-    console.log("You won!")
-    ++playerRWins;
-    return 2;
-  }
-  else {
-    return 0; //error
-  }
-}
-
-//let gameQ = prompt("Would you like to play a game? y/n");
-
-function round() {
-  playerTurn();
-  aiTurn();
-}
-
-function match() {
-  do {
-    round();
-    console.log("You picked " + c2I(choice) + " and I picked " + c2I(jsChoice) + ".");
-    alert("You picked " + c2I(choice) + " and I picked " + c2I(jsChoice) + ".");
-    compareChoices(choice, jsChoice);
-    console.log("The score is " + playerRWins + "(you) to " + aiRWins + "(me)")
-    alert("The score is " + playerRWins + "(you) to " + aiRWins + "(me)")
-  } while (playerRWins < 5 && aiRWins < 5);
-
-  if (playerRWins == 5) {
-    console.log("You won the match!")
-    alert("You won the match!")
-    ++playerMWins;
-  }
-  else if (aiRWins == 5) {
-    console.log("You lost the match!")
-    alert("You lost the match!")
-    ++aiMWins;
-  }
-
-  console.log("Matches won: " + playerMWins + "(you) to " + aiMWins + "(me)")
-  alert("Matches won: " + playerMWins + "(you) to " + aiMWins + "(me)")
-}
-
-// main loop below
-do {
-  playerRWins = 0;
-  aiRWins = 0;
-  match();
-} while (prompt("would you like to play a game? y/n") == "y");
-
-/*console.log(choice)
-console.log(jsChoice)*/
+playGame();
